dae35ff26a5e27379bd46eb3c849154038555632
Avinash <avinashsri1605@gmail.com>
2023-06-06 08:55:20 +0530

    changes in view but problem in posting json

--- customer/views.py
+++ customer/views.py
@@ -4,21 +4,75 @@ from rest_framework import status
 from .serializers import CartSerializer
 from .models import *
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 def handlingCart(request):
-
     if request.method == 'GET':
-        carts = Cart.objects.all()  # Assuming you have a Cart model
+        carts = Cart.objects.all()
         serializer = CartSerializer(carts, many=True)
         return Response(serializer.data)
     
     if request.method == 'POST':
-        profile = request.user.profile
-        user_id = profile.user_id
-        user_obj = Profile.objects.filter(user_id=user_id)
-        item_id = Item.id
-        serializer = CartSerializer(data=request.data)
+        enrollment_no = request.data.get('enrollment_no')
+        item_id = request.data.get('item_id')
+        quantity = request.data.get('quantity')
+        
+        try:
+            profile = Profile.objects.get(enrollment_no=enrollment_no)
+        except Profile.DoesNotExist:
+            return Response({'error': 'Invalid enrollment number'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            item = Item.objects.get(id=item_id)
+        except Item.DoesNotExist:
+            return Response({'error': 'Invalid item ID'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        cart_data = {
+            'profile': profile.enrollment_no,
+            'item': item.id,
+            'quantity': quantity
+        }
+        
+        serializer = CartSerializer(data=cart_data)
         if serializer.is_valid():
             serializer.save()
+            print(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# @api_view(['POST','GET'])
+# def handlingCart(request):
+#     if request.method == 'GET':
+#         carts = Cart.objects.all()
+#         serializer = CartSerializer(carts, many=True)
+#         return Response(serializer.data)
+
+#     if request.method == 'POST':
+#         profile_data = request.data.get('profile')
+#         item_data = request.data.get('item')
+#         user_id = request.data.get('user')
+#         quantity = request.data.get('quantity')
+
+#         try:
+#             profile = Profile.objects.get(enrollment_no=profile_data['enrollment_no'])
+#         except Profile.DoesNotExist:
+#             return Response({'error': 'Invalid enrollment number'}, status=status.HTTP_400_BAD_REQUEST)
+
+#         try:
+#             item = Item.objects.get(id=item_data['id'])
+#         except Item.DoesNotExist:
+#             return Response({'error': 'Invalid item ID'}, status=status.HTTP_400_BAD_REQUEST)
+
+#         cart_data = {
+#             'profile': profile.enrollment_no,
+#             'item': item.id,
+#             'user': user_id,
+#             'quantity': quantity
+#         }
+
+#         serializer = CartSerializer(data=cart_data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
